5504c718369615642dd4cf8c7f518c0ea8514045
bhumesh2001 <bhumesh21@navgurukul.org>
2024-06-28 23:53:13 +0530

    new commit

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,3 +1,28 @@
+const socket = io('https://mailsender-backend-application.onrender.com');
+
+let log = document.getElementById('log');
+let ptag = document.createElement('p');
+
+socket.on('connect', () => {
+    console.log('Connected to the server');
+});
+
+socket.on('log', (message) => {
+    const Ptag = document.createElement('p');
+    if (message.success) {
+        Ptag.textContent = message.mesg;
+        log.appendChild(Ptag);
+    } else {
+        Ptag.textContent = message.mesg;
+        Ptag.classList.add('text-danger');
+        log.appendChild(Ptag);
+    };
+});
+
+socket.on('disconnect', () => {
+    console.log('Disconnected from the server');
+});
+
 document.getElementById('myForm').addEventListener('submit', async (event) => {
     event.preventDefault();
 
@@ -12,17 +37,12 @@ document.getElementById('myForm').addEventListener('submit', async (event) => {
     const button = document.getElementById('my-btn');
     const spinner = document.getElementById('spinner');
     const buttonText = document.getElementById('btn-text');
-    let log = document.getElementById('log');
-    let ptag = document.createElement('p');
     const show_result = document.getElementById('show-mail');
     const mail_msg = document.getElementById('w-msg');
 
     button.disabled = true;
     spinner.style.display = 'inline-block';
     buttonText.style.display = 'none';
-
-    ptag.textContent = 'Please wait for the responses...';
-    log.appendChild(ptag);
 
     let file = document.getElementById('resume').files[0];
     if (file !== undefined) {
@@ -44,7 +64,6 @@ document.getElementById('myForm').addEventListener('submit', async (event) => {
     } else {
         await sendDataToServer(formData);
     };
-
     async function sendDataToServer(formData) {
         let response = await fetch('https://mailsender-backend-application.onrender.com/mail', {
             method: 'POST',
@@ -58,24 +77,12 @@ document.getElementById('myForm').addEventListener('submit', async (event) => {
             button.disabled = false;
             spinner.style.display = 'none';
             buttonText.style.display = 'inline-block';
-            log.removeChild(ptag);
-
-            res.Res.forEach(element => {
-                const Ptag = document.createElement('p');
-                Ptag.textContent = element;
-                log.appendChild(Ptag);
-            });
-
-            res.err.forEach(ele => {
-                const Ptag = document.createElement('p');
-                Ptag.textContent = ele;
-                Ptag.classList.add('text-danger');
-                log.appendChild(Ptag);
-            });
 
             mail_msg.textContent = `${res.mailCount} mail has been sent.`;
             show_result.classList.remove('d-none');
             show_result.classList.add('d-block');
+        } else {
+            console.log(res.error);
         };
     };
 });
