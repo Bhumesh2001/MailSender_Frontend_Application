5b43726b552d8e43fd79bc7c288e167f301cbb33
bhumesh2001 <bhumesh21@navgurukul.org>
2024-07-12 20:38:46 +0530

    initial

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,4 +1,4 @@
-const socket = io('https://mailsender-backend-application.onrender.com');
+const socket = io('http://localhost:5000');
 
 let log = document.getElementById("log");
 let ptag = document.createElement("p");
@@ -15,6 +15,7 @@ socket.on("connect", () => {
 
 socket.on("log", (message) => {
   console.log(message, '========>');
+  // if(message.userId === userId){}
   const Ptag = document.createElement("p");
   if (message.success) {
     Ptag.textContent = message.mesg;
@@ -83,7 +84,7 @@ document.getElementById("myForm").addEventListener("submit", async (event) => {
     await sendDataToServer(formData);
   }
   async function sendDataToServer(formData) {
-    let response = await fetch("https://mailsender-backend-application.onrender.com/mail", {
+    let response = await fetch("http://localhost:5000/mail", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
